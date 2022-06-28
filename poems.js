d3141ecb2d6354548d1a7e98a5904b238d0fd72d
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2022-06-27 21:24:04 -0700

    Create poems.js

---
+++ poems.js
@@ -0,0 +1,14 @@
+poems: {
+    [
+        line1 = "wildflower countryside",
+        line2 = "waving back",
+        line3 = "at me",
+        img = "/images/9.png"
+    ],
+    [
+        line1 = "wildflower countryside",
+        line2 = "waving back",
+        line3 = "at me",
+        img = "/images/9.png"
+    ]
+}
